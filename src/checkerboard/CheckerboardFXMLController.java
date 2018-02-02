26205f857cf132bb70906599448b470a6864f990
alxgmpr <agompper@me.com>
2018-02-02 16:52:49 -0600

    First commit (project setup)

---
+++ src/checkerboard/CheckerboardFXMLController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkerboard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author alex
+ */
+public class CheckerboardFXMLController implements Initializable, Startable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+    
+}
